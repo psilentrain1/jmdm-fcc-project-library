1835ea99cc52edeb18103d924f0fcd67fafae1f7
James Drake <info@jamesmdrakemedia.com>
2025-04-26 06:10:04 -0400

    feat: set up api

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,94 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB);
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  commentcount: { type: Number, default: 0 },
+  comments: [String],
+});
+
+const Book = mongoose.model("Book", bookSchema);
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(async function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      const books = await Book.find({}, "_id title commentcount");
+      if (books.length === 0) return res.status(204).send("No books found");
+      res.json(books);
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+
+    .post(async function (req, res) {
+      const title = req.body.title;
+      if (!title || title === "") return res.status(400).send("missing required field title");
+      const newBook = new Book({ title: title });
+      try {
+        const savedBook = await newBook.save();
+        res.status(201).json({ _id: savedBook._id, title: savedBook.title });
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(async function (req, res) {
       //if successful response will be 'complete delete successful'
+      try {
+        const deletedBooks = await Book.deleteMany({});
+        if (deletedBooks.deletedCount === 0) return res.status(404).send("no books exist");
+        res.status(200).send("complete delete successful");
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+  app
+    .route("/api/books/:id")
+    .get(async function (req, res) {
+      const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      const book = await Book.findById(bookid, "_id title comments");
+      if (!book) return res.status(404).send("no book exists");
+      res.json(book);
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
+
+    .post(async function (req, res) {
+      const bookid = req.params.id;
+      const comment = req.body.comment;
       //json res format same as .get
+      const book = await Book.findById(bookid);
+      if (!book) return res.status(404).send("no book exists");
+      if (!comment || comment === "") return res.status(400).send("missing required field comment");
+      book.comments.push(comment);
+      book.commentcount = book.comments.length;
+      try {
+        const savedBook = await book.save();
+        res.status(200).json({ _id: savedBook._id, title: savedBook.title, comments: savedBook.comments });
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
+
+    .delete(async function (req, res) {
+      const bookid = req.params.id;
       //if successful response will be 'delete successful'
+      try {
+        const deletedBook = await Book.findByIdAndDelete(bookid);
+        if (!deletedBook) return res.status(404).send("no book exists");
+        res.status(200).send("delete successful");
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
-  
 };
